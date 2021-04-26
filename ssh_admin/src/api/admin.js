6b3c18ae3f44ec6da47363baec19f40a80a1b718
fantasyzernontra <fantasyzernontra@hotmail.com>
2021-04-26 18:05:26 +0700

    [ADD] Admin Actions Trigger

--- ssh_admin/src/api/admin.js
+++ ssh_admin/src/api/admin.js
@@ -2,7 +2,12 @@ import API from "../api";
 
 export default {
   login: async (username, password) => {
-    return API.post("login")
+    return API.post("/login")
+      .then((res) => res)
+      .catch((err) => err);
+  },
+  logout: async (username) => {
+    return API.post("/logout")
       .then((res) => res)
       .catch((err) => err);
   },
