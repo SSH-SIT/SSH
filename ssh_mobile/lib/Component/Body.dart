b976c2566c093177b00570100e288d23c8dc9f1c
Mek <supagon.sit@mail.kmutt.ac.th>
2021-04-27 14:45:58 +0700

    [Update]body

--- ssh_mobile/lib/Component/Body.dart
+++ ssh_mobile/lib/Component/Body.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:ssh_mobile/Component/BotNavBar.dart';
 import 'package:ssh_mobile/constant.dart';
 
+import 'Categories.dart';
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,7 +13,7 @@ class Body extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
           child: Text(
-            "Catagories",
+            "Categories",
             style: Theme.of(context)
                 .textTheme
                 .headline5
@@ -19,66 +21,12 @@ class Body extends StatelessWidget {
           ),
         ),
         Categories(),
+        Container(
+          height: 180,
+          width: 160,
+          decoration: BoxDecoration(color: Colors.blue),
+        )
       ],
-    );
-  }
-}
-
-class Categories extends StatefulWidget {
-  @override
-  _CategoriesState createState() => _CategoriesState();
-}
-
-class _CategoriesState extends State<Categories> {
-  List<String> categories = [
-    "Wand Vibrators",
-    "Clitoral Vibrator",
-    "Clit Suction Toys",
-    "Dildos"
-  ];
-  int selectedIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin),
-      child: SizedBox(
-        height: 25,
-        child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: categories.length,
-            itemBuilder: (context, index) => buildCategory(index)),
-      ),
-    );
-  }
-
-  Widget buildCategory(int index) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          selectedIndex = index;
-        });
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              categories[index],
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: selectedIndex == index ? kTextColor : kTextLightColor,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: kDefaultPaddin / 4), // top padding
-              height: 2,
-              width: 30,
-              color: selectedIndex == index ? Colors.black : Colors.transparent,
-            )
-          ],
-        ),
-      ),
     );
   }
 }
